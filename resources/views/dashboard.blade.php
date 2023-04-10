94ae3efdee4fd078d06048e1f5b5a0e757f1d42f
SALMA ANNISSA AZIZI <salmaannisaazizi@gmail.com>
2023-04-10 20:50:43 +0700

    first commit

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,14 @@
+@extends ('layouts.template')
+
+@section('content')
+<section class="content">
+
+    <!-- Default Box-->
+    <div class="card">
+        <div class="card-header">
+            <h3 class="card-title"> Halo Selamat Datang </h3>
+            <br>
+        </div>
+    </div>
+</section>
+@endsection
